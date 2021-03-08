5698a6d146560ac4ad5990136d5281223770edc7
TSJ-Arita <80231573+TSJ-Arita@users.noreply.github.com>
2021-03-08 18:52:28 +0900

    増やしてみた

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("R")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("L")
+})
+let Start = 0
+let 方向 = 0
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
+    方向 = 0
+    Start = 0
+    while (true) {
+    	
+    }
+})
+control.inBackground(function () {
 	
 })
