9d282bd71f85fbd0b0ed8dc26fb15ccd55b3aad5
TSJ-Arita <80231573+TSJ-Arita@users.noreply.github.com>
2021-03-18 15:09:29 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,15 +4,8 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showString("L")
 })
-let Start = 0
-let 方向 = 0
-basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    方向 = 0
-    Start = 0
-    while (true) {
-    	
-    }
+    basic.showIcon(IconNames.Heart)
 })
 control.inBackground(function () {
 	
