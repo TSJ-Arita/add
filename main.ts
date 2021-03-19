4b02a9cb7ea6dc5dee0441a29a54cf12e2aab54d
TSJ-Arita <80231573+TSJ-Arita@users.noreply.github.com>
2021-03-19 18:58:08 +0900

    最適化のベースっす

--- main.ts
+++ main.ts
@@ -1,24 +1,35 @@
+maqueen.ltEvent(maqueen.Patrol1.PatrolRight, maqueen.Voltage.Low, function () {
+    _R_SPD = 255
+})
+maqueen.ltEvent(maqueen.Patrol1.PatrolRight, maqueen.Voltage.High, function () {
+    _R_SPD = 127
+})
 input.onButtonPressed(Button.A, function () {
     Start = 1
     basic.showString("R")
 })
 maqueen.ltEvent(maqueen.Patrol1.PatrolLeft, maqueen.Voltage.High, function () {
-	
+    _L_SPD = 127
 })
 input.onButtonPressed(Button.B, function () {
     Start = 2
     basic.showString("L")
 })
+maqueen.ltEvent(maqueen.Patrol1.PatrolLeft, maqueen.Voltage.Low, function () {
+    _L_SPD = 255
+})
+let _L_SPD = 0
+let _R_SPD = 0
 let Start = 0
 Start = 0
+_R_SPD = 255
+_L_SPD = 255
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-strip.showColor(neopixel.colors(NeoPixelColors.White))
+strip.showColor(neopixel.colors(NeoPixelColors.Red))
 basic.pause(2000)
-let READERBUFFER_SIZE = 4
-let WRITERBUFFER_SIZE = 4
-let empty_list = [READERBUFFER_SIZE]
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, _R_SPD)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, _L_SPD)
     basic.showString("" + Start)
     if (Start == 1) {
     	
