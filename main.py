90be652719f35805e6766231256c5abb1347f8e2
TSJ-Arita <80231573+TSJ-Arita@users.noreply.github.com>
2021-03-20 15:09:29 +0900

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ def on_button_pressed_a():
     basic.show_string("R")
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def my_function():
+def doSomething():
     pass
-maqueen.lt_event(maqueen.Patrol1.PATROL_LEFT,
-    maqueen.Voltage.HIGH,
-    my_function)
 
 def on_button_pressed_b():
     global Start
@@ -17,23 +14,19 @@ def on_button_pressed_b():
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 Start = 0
-Start = 0
+Start = 1
+_R_SPD = 51
+_L_SPD = 34
 strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-strip.show_color(neopixel.colors(NeoPixelColors.WHITE))
+basic.show_string("" + str(Start))
 basic.pause(2000)
-READERBUFFER_SIZE = 4
-WRITERBUFFER_SIZE = 4
-empty_list = [READERBUFFER_SIZE]
 
 def on_forever():
-    maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 255)
-    basic.show_string("" + str(Start))
-    if Start == 1:
-        pass
+    global _L_SPD
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, _L_SPD)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, _R_SPD)
+    if maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
+        _L_SPD = 0
     else:
-        pass
+        _L_SPD = 34
 basic.forever(on_forever)
-
-def on_in_background():
-    pass
-control.in_background(on_in_background)
--- main.ts
+++ main.ts
@@ -1,42 +1,24 @@
-maqueen.ltEvent(maqueen.Patrol1.PatrolRight, maqueen.Voltage.Low, function () {
-    _R_SPD = 255
-})
-maqueen.ltEvent(maqueen.Patrol1.PatrolRight, maqueen.Voltage.High, function () {
-    _R_SPD = 127
-})
 input.onButtonPressed(Button.A, function () {
     Start = 1
     basic.showString("R")
-})
-maqueen.ltEvent(maqueen.Patrol1.PatrolLeft, maqueen.Voltage.High, function () {
-    _L_SPD = 127
 })
 input.onButtonPressed(Button.B, function () {
     Start = 2
     basic.showString("L")
 })
-maqueen.ltEvent(maqueen.Patrol1.PatrolLeft, maqueen.Voltage.Low, function () {
-    _L_SPD = 255
-})
-let _L_SPD = 0
-let _R_SPD = 0
 let Start = 0
-Start = 0
-_R_SPD = 255
-_L_SPD = 255
+Start = 1
+let _R_SPD = 51
+let _L_SPD = 34
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-strip.showColor(neopixel.colors(NeoPixelColors.Red))
+basic.showString("" + Start)
 basic.pause(2000)
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, _R_SPD)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, _L_SPD)
-    basic.showString("" + Start)
-    if (Start == 1) {
-    	
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, _R_SPD)
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        _L_SPD = 0
     } else {
-    	
+        _L_SPD = 34
     }
-})
-control.inBackground(function () {
-	
 })
