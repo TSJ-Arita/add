8c2d694d566fb69333b41a0d5ed29bc2de086ae5
TSJ-Arita <80231573+TSJ-Arita@users.noreply.github.com>
2021-03-23 00:16:17 +0900

    うまく回れることもある、判定まだ

--- main.ts
+++ main.ts
@@ -1,13 +1,5 @@
-input.onButtonPressed(Button.A, function () {
-    Start = 1
-    basic.showString("R")
-})
-input.onButtonPressed(Button.B, function () {
-    Start = 2
-    basic.showString("L")
-})
-function chokobo () {
-    music.setTempo(150)
+function chokobo1 () {
+    music.setTempo(39)
     music.playTone(587, music.beat(BeatFraction.Eighth))
     music.rest(music.beat(BeatFraction.Eighth))
     music.playTone(494, music.beat(BeatFraction.Eighth))
@@ -37,14 +29,57 @@ function chokobo () {
     music.playTone(392, music.beat(BeatFraction.Sixteenth))
     music.rest(music.beat(BeatFraction.Sixteenth))
     music.playTone(392, music.beat(BeatFraction.Sixteenth))
-    music.playTone(330, music.beat(BeatFraction.Sixteenth))
+    music.playTone(494, music.beat(BeatFraction.Sixteenth))
     music.playTone(587, music.beat(BeatFraction.Sixteenth))
     music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(659, music.beat(BeatFraction.Sixteenth))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(698, music.beat(BeatFraction.Half))
 }
+input.onButtonPressed(Button.A, function () {
+    Start = 1
+    basic.showString("R")
+})
+function chokobo2 () {
+    music.playTone(659, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.playTone(440, music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Eighth))
+    music.playTone(440, music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.playTone(659, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(784, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Half))
+    music.playTone(494, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.playTone(440, music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Eighth))
+    music.playTone(440, music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Sixteenth))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(494, music.beat(BeatFraction.Sixteenth))
+    music.playTone(523, music.beat(BeatFraction.Sixteenth))
+    music.playTone(494, music.beat(BeatFraction.Sixteenth))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(440, music.beat(BeatFraction.Sixteenth))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(494, music.beat(BeatFraction.Half))
+}
+input.onButtonPressed(Button.B, function () {
+    Start = 2
+    basic.showString("L")
+})
 let _L_SPD = 0
 let _R_SPD = 0
 let Start = 0
-Start = 1
+Start = 0
 let debug = 1
 if (debug) {
     _R_SPD = 51
@@ -59,9 +94,23 @@ basic.pause(2000)
 basic.forever(function () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, _L_SPD)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, _R_SPD)
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, _L_SPD)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, _R_SPD)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, _L_SPD)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, _R_SPD)
+    }
     if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        _L_SPD = 0
+        _L_SPD = 11
     } else {
         _L_SPD = 34
     }
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+        _R_SPD = 17
+    } else {
+        _R_SPD = 51
+    }
+})
+control.inBackground(function () {
+	
 })
